d01cbe03f129397791cae56cf61815dd010b2813
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-19 11:58:03 +0600

    Update test_sql_data.py

--- tests/test_sql_data.py
+++ tests/test_sql_data.py
@@ -2,7 +2,7 @@ import pymssql
 import pytest
 
 # Database connection details
-DB_SERVER = "host.docker.internal"  # Change if needed
+DB_SERVER = "localhost\\SQLEXPRESS"  # Change if needed
 DB_USER = "aktanbek"  # Your SQL Server username
 DB_PASSWORD = "Arzymamat5"  # Your SQL Server password
 DB_DATABASE = "AdventureWorks2022"  # Your database name

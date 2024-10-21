687d7ae4366f82798963ffa42c07a3344813e925
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-21 10:40:11 +0600

    Update test_sql_data.py

--- tests/test_sql_data.py
+++ tests/test_sql_data.py
@@ -2,7 +2,7 @@ import pymssql
 import pytest
 
 # Database connection details
-DB_SERVER = "EPKGBISW0047"  # Change if needed
+DB_SERVER = "EPKGBISW0047\\SQLEXPRESS"  # Change if needed
 DB_USER = "aktanbek"  # Your SQL Server username
 DB_PASSWORD = "Arzymamat5"  # Your SQL Server password
 DB_DATABASE = "AdventureWorks2022"  # Your database name

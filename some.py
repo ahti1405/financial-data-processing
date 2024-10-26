36c6e686f89b9d7610ce5f2c0cd1fd7e2d84ac3f
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-26 21:45:21 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,1 +1,8 @@
-print("Hey, this is only for tetsting purpose")
+import pymssql
+
+conn = pymssql.connect(server="192.168.86.3", port="1433", user="aktanbek", password="Arzymamat5", database="AdventureWorks2022")
+cursor = conn.cursor()
+cursor.execute("SELECT 1")
+print("Connection successful") if cursor.fetchone() else print("Connection failed")
+conn.close()
+

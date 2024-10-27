4aa85bd0ef5a2d460df6900ec4941f7885c3eaca
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-27 19:57:12 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,7 +1,7 @@
 import pymssql
 
 try:
-    conn = pymssql.connect(server="192.168.86.3", port="1433", user="aktanbek", password="Arzymamat5", database="AdventureWorks2022")
+    conn = pymssql.connect(server="EPKGBISW0047.mshome.net", port="1433", user="aktanbek", password="Arzymamat5", database="AdventureWorks2022")
     cursor = conn.cursor()
     cursor.execute("SELECT 1")
     print("Connection successful") if cursor.fetchone() else print("Connection failed")

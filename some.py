6fb1682c743e5addbb291e52bc2ecbe2af5a4b8d
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-27 22:15:57 +0600

    Update some.py

--- some.py
+++ some.py
@@ -12,3 +12,30 @@ except pymssql.OperationalError as e:
     print(f"Operational error: {e}")
 except Exception as e:
     print(f"Unexpected error: {e}")
+
+try:
+    conn = pymssql.connect(server="127.0.0.1", port="1433", user="TestLogin", password="Arzymamat5", database="AdventureWorks2022")
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1")
+    print("Connection successful") if cursor.fetchone() else print("Connection failed")
+    conn.close()
+except pymssql.InterfaceError as e:
+    print(f"Interface error: {e}")
+except pymssql.OperationalError as e:
+    print(f"Operational error: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+
+try:
+    conn = pymssql.connect(server="172.25.160.1", port="1433", user="TestLogin", password="Arzymamat5", database="AdventureWorks2022")
+    cursor = conn.cursor()
+    cursor.execute("SELECT 1")
+    print("Connection successful") if cursor.fetchone() else print("Connection failed")
+    conn.close()
+except pymssql.InterfaceError as e:
+    print(f"Interface error: {e}")
+except pymssql.OperationalError as e:
+    print(f"Operational error: {e}")
+except Exception as e:
+    print(f"Unexpected error: {e}")
+

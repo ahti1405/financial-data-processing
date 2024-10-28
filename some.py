67756e63b2c31916fc543a82222d370ec8a5317d
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-28 23:41:07 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,14 +1,13 @@
 import pymssql
 
-conn = pymssql.connect(
-    server='192.168.0.103',
-    user='aktanbek',
-    password='Arzymamat5',
-    database='AdventureWorks2022',
-    as_dict=True
-)
-cursor = conn.cursor()
-cursor.execute('SELECT TOP 10 * FROM person.person')
-records = cursor.fetchall()
-print(records)
-conn.close()
+with pymssql.connect(server='192.168.0.103',
+                     database='TRN',
+                     user='aktanbek',
+                     password='Arzymamat5',
+                     as_dict=True) as conn:
+    print("Connection established successfully.")
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT 1 AS test_column')
+    result = cursor.fetchone()
+    print("Query executed su

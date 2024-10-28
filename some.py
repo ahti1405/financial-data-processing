b6b5bb7eef4f9baafd640b273720b81a836f955f
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-28 16:53:46 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,1 +1,13 @@
-print("hello")
+import pymssql
+
+with pymssql.connect(server='192.168.0.103',
+                     database='AdventureWorks2022',
+                     user='aktanbek',
+                     password='Arzymamat5',
+                     as_dict=True) as conn:
+    print("Connection established successfully.")
+
+    cursor = conn.cursor()
+    cursor.execute('SELECT 1 AS test_column')
+    result = cursor.fetchone()
+    print("Query executed successfully:", r

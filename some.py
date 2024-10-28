220508f7afe53edbbec36614a6e288becf88b9f1
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-10-28 23:54:30 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,6 +1,6 @@
 import pymssql
 
-with pymssql.connect(server='172.25.160.1',
+with pymssql.connect(server='127.0.0.1',
                      database='TRN',
                      user='aktanbek',
                      password='Arzymamat5',

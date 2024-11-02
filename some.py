bf042fdf6b98e0a9faf953fe1c690cfc80feddbb
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-11-02 14:53:28 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,6 +1,6 @@
 import pymssql
 
-with pymssql.connect(server='localhost\SQLEXPRESS',
+with pymssql.connect(server='192.168.0.104\SQLEXPRESS,1433',
                      port='1433',
                      database='TRN',
                      user='aktanbek',

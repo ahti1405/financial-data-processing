398a0c62af548b4eb04d3389d5564f4d0dc64a15
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-11-02 11:32:54 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,11 +1,12 @@
 import pymssql
 
-with pymssql.connect(server='192.168.0.104\\SQLEXPRESS',
+with pymssql.connect(server='192.168.0.104',
                      port='1433',
                      database='TRN',
                      user='aktanbek',
                      password='Arzymamat5',
-                     as_dict=True) as conn:
+                     as_dict=True,
+                    timeout=30) as conn:
     print("Connection established successfully.")
 
     cursor = conn.cursor()

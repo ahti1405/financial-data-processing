60a258ce787b5d43679e9b32fb9ab5aace64591e
Aktanbek Aidarov <88418838+ahti1405@users.noreply.github.com>
2024-11-07 23:35:46 +0600

    Update some.py

--- some.py
+++ some.py
@@ -1,19 +1,14 @@
 import pymssql
 
-# Connect using Windows Authentication
-connection = pymssql.connect(server='192.168.0.104',
-                             database='TRN',
-                             autocommit=True)
+with pymssql.connect(server='192.168.0.103',
+                     database='AdventureWorks2022',
+                     user='aktanbek',
+                     password='Arzymamat5'
 
-# Optional: Set the connection to enforce encryption if needed
-# Note: pymssql does not have a built-in parameter for SSL in the connection string.
-# This might require custom configurations on the server and client side.
+                     ) as conn:
+    print("Connection established successfully.")
 
-try:
-    cursor = connection.cursor()
+    cursor = conn.cursor()
     cursor.execute('SELECT 1 AS test_column')
     result = cursor.fetchone()
     print("Query executed successfully:", result)
-finally:
-    connection.close()
-
